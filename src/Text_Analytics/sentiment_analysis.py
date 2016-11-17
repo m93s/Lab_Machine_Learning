8d78bec997a047771f51831801375cfcda9f481b
mayank <mayank.socnet@gmail.com>
2016-11-17 20:34:32 +0530

    sample

--- src/Text_Analytics/sentiment_analysis.py
+++ src/Text_Analytics/sentiment_analysis.py
@@ -3,8 +3,13 @@ import os
 
 import csv
 
+
+
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk.corpus.reader.plaintext import CategorizedPlaintextCorpusReader
+
+from nltk.classify.naivebayes import NaiveBayesClassifier
+from exceptions import UnicodeDecodeError
 
 corpus_dir =  '/home/mayank/IdeaProjects/Lab_Machine_Learning/src/Text_Analytics/data/rt-polaritydata'
 
@@ -18,8 +23,24 @@ cat_map_ = {
 fileids_ = '^rt-polarity.*'
 
 categorized_plaintext_corpusreader = CategorizedPlaintextCorpusReader(
-    root = corpus_dir, cat_map = cat_map_ , fileids = fileids_)
+    root = corpus_dir, cat_map = cat_map_ , fileids = fileids_,
+    )
 
-print categorized_plaintext_corpusreader.categories()
 
-pos_view = categorized_plaintext_corpusreader.words(categories='pos')
+# pos_view = categorized_plaintext_corpusreader.words(categories='pos')
+
+
+
+# classification
+sents = categorized_plaintext_corpusreader.sents()
+
+for steam_view in sents:
+    steam_view.close()
+
+for  senetence in sents:
+    try:
+        print senetence
+    except UnicodeDecodeError:
+        print 'error'
+
+# classifier = NaiveBayesClassifier.train()
