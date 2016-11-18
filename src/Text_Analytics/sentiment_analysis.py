d4dee915ed7db87ba59f2fee006fda2d63e282ba
mayank <mayank.socnet@gmail.com>
2016-11-18 19:02:22 +0530

    sample

--- src/Text_Analytics/sentiment_analysis.py
+++ src/Text_Analytics/sentiment_analysis.py
@@ -3,8 +3,8 @@ import os
 
 import csv
 
-
-
+import unicodedata
+import codecs
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk.corpus.reader.plaintext import CategorizedPlaintextCorpusReader
 
@@ -18,6 +18,28 @@ cat_map_ = {
     'rt-polarity.neg' : ['neg']
 }
 
+def unicode_normalization(file_path):
+    source_file = codecs.open(file_path,mode = 'r',
+                              encoding = 'latin-1', errors = 'ignore')
+    target_file = codecs.open(file_path.rpartition('/')[0]
+                + '/un_'
+                + file_path.rpartition('/')[-1],
+                mode = 'w',
+                encoding = 'utf-8',
+                errors = 'ignore')
+    while True:
+        contents = source_file.read()
+        if not contents:
+            break
+        target_file.write(contents)
+
+def corpus_treatment(root):
+
+    corpus_dir = root
+
+    files =
+    return None
+
 # fileids_ = corpus_dir + '/rt-polarity*'
 
 fileids_ = '^rt-polarity.*'
@@ -25,6 +47,14 @@ fileids_ = '^rt-polarity.*'
 categorized_plaintext_corpusreader = CategorizedPlaintextCorpusReader(
     root = corpus_dir, cat_map = cat_map_ , fileids = fileids_,
     )
+
+pos_words = categorized_plaintext_corpusreader.words(categories=['pos'])
+pos_sents = categorized_plaintext_corpusreader.sents(categories=['pos'])
+pos_paras = categorized_plaintext_corpusreader.paras(categories=['pos'])
+
+neg_words = categorized_plaintext_corpusreader.words(categories=['pos'])
+neg_sents = categorized_plaintext_corpusreader.sents(categories=['neg'])
+neg_paras = categorized_plaintext_corpusreader.paras(categories=['neg'])
 
 
 # pos_view = categorized_plaintext_corpusreader.words(categories='pos')
